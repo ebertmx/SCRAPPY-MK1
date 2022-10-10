b2c914338257a5b85095666782a067ab9182a720
ebertmx <87283949+ebertmx@users.noreply.github.com>
2022-10-10 13:18:08 -0700

    Added Wifi Connect

--- src/SCRAPPY_CONTROLLER/main/main.h
+++ src/SCRAPPY_CONTROLLER/main/main.h
@@ -20,3 +20,57 @@ static const char *MainTAG = "MainProgram";
 
 #define INCLUDE_vTaskSuspend 1
 
+//WIFI STUFF
+
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#define EXAMPLE_ESP_WIFI_SSID      "Ebert Farm"
+#define EXAMPLE_ESP_WIFI_PASS      "6704Palfrey"
+#define EXAMPLE_ESP_MAXIMUM_RETRY  5
+
+
+
+// #if CONFIG_ESP_WIFI_AUTH_OPEN
+// #define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_OPEN
+// #elif CONFIG_ESP_WIFI_AUTH_WEP
+// #define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WEP
+// #elif CONFIG_ESP_WIFI_AUTH_WPA_PSK
+// #define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA_PSK
+// #elif CONFIG_ESP_WIFI_AUTH_WPA2_PSK
+// #define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_PSK
+// #elif CONFIG_ESP_WIFI_AUTH_WPA_WPA2_PSK
+// #define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA_WPA2_PSK
+// #elif CONFIG_ESP_WIFI_AUTH_WPA3_PSK
+// #define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA3_PSK
+// #elif CONFIG_ESP_WIFI_AUTH_WPA2_WPA3_PSK
+// #define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_WPA3_PSK
+// #elif CONFIG_ESP_WIFI_AUTH_WAPI_PSK
+// #define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WAPI_PSK
+// #endif
+
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA_PSK
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+
+static const char *TAG = "wifi station";
+static int s_retry_num = 0;
+
